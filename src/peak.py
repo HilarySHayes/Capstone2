35646d7626f51a5721d30dd590acb02e75d60cea
Hilary Hayes <sarneski@gmail.com>
2021-03-31 11:11:31 -0600

    Improved modularity

--- src/peak.py
+++ src/peak.py
@@ -20,39 +20,43 @@ def peak_detector(df, gain_threshold):
 
 def plot_one_cluster(m, df, activities_ids, steps, color):
     for activity_id in activities_ids:
-        locations = df.query(f"activity_id=={activity_id}")[['position_lat', 'position_long']].iloc[::steps]
+        locations = df.query(f'activity_id=={activity_id}')[['position_lat', 'position_long']].iloc[::steps]
         locations.dropna(inplace=True)
         points = locations.values.tolist()
         folium.PolyLine(points, color=color, weight=2.5, opacity=0.5).add_to(m)
 
-
 def plot_multiple_clusters(df, peaks, central_lat, central_long, colormap, cluster_series, steps):
-    m = folium.Map(location=[central_lat,central_long],tiles="Stamen Terrain",zoom_start=12)
+    m = folium.Map(location=[central_lat, central_long],tiles='Stamen Terrain', zoom_start=12)
     n = len(list(cluster_series.index))
     peak_positions = peaks.groupby('cluster')[['position_lat', 'position_long']].mean()
     for i, val in enumerate(list(cluster_series.index)):
         color = next(colormap)
-        activities_ids = list(peaks.query(f"cluster=={val}").index)
+        activities_ids = list(peaks.query(f'cluster=={val}').index)
         plot_one_cluster(m, df, activities_ids, steps, color)
         peak = peak_positions.loc[val]
         folium.CircleMarker(location=[peak['position_lat'], peak['position_long']],radius=10, color=color).add_to(m)
     return m
 
-if __name__ == '__main__':
-    person = "KM_Strava"
-    person_df = pd.read_parquet(f"../data/{person}/df.parquet")
-    peaks = peak_detector(person_df, 400)
-    peaks = peaks.dropna(subset=['position_lat', 'position_long']) 
-    epsilon = 0.0005
-    clustering = DBSCAN(eps=epsilon, metric='haversine', min_samples=2).fit(peaks[['position_lat', 'position_long']])
+def peak_clustering(peaks, epsilon, min_samples=2):
+    clustering = DBSCAN(eps=epsilon, metric='haversine', min_samples=min_samples).fit(peaks[['position_lat', 'position_long']])
     peaks['cluster'] = clustering.labels_ 
     frequency = peaks['cluster'].value_counts().sort_values(ascending=False)
     cluster_series = frequency[frequency.index != -1]
+    return cluster_series
 
+def save_cluster_plot(person, epsilon, central_lat, central_long, colormap, filename, gain_threshold=400, steps=10):
+    person_df = pd.read_parquet(f'../data/{person}/df.parquet')
+    peaks = peak_detector(person_df, gain_threshold)
+    peaks = peaks.dropna(subset=['position_lat', 'position_long'])
+    cluster_series = peak_clustering(peaks, epsilon)
+    m = plot_multiple_clusters(person_df, peaks, central_lat, central_long, colormap, cluster_series, steps)
+    m.save(f'../html/{filename}.html')  
+
+if __name__ == '__main__':
+    person = 'KM_Strava'
+    epsilon = 0.0005
     colors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f',
           '#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
     colormap = itertools.cycle(colors)
     central_lat, central_long = 39.5744, -106.0975
-
-    m = plot_multiple_clusters(person_df, peaks, central_lat, central_long, colormap, cluster_series, 10)
-    m.save(f'../html/testmap.html')
+    save_cluster_plot(person, epsilon, central_lat, central_long, colormap, 'Summit')
