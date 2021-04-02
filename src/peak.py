c0d6d335f72e75e2c14af48fc46bd0d0911e6fe9
Hilary Hayes <sarneski@gmail.com>
2021-04-02 08:30:32 -0600

    Added docustrings

--- src/peak.py
+++ src/peak.py
@@ -5,6 +5,15 @@ import pandas as pd
 from sklearn.cluster import DBSCAN
 
 def max_altitude(act):
+    """[Find the highest point in an activity]
+    Args:
+        act: [pandas dataframe for activity
+                with altitude column]
+    Returns:
+        highest_pt: [panda dataframe with single row corresponding 
+        to when the highest point was acheived. Has new column gain 
+        marking the difference between the highest pt and lowest point]
+    """
     index = act['altitude'].idxmax()
     diff = act['altitude'].max() - act['altitude'].min()
     highest_pt = act.loc[index]
@@ -12,13 +21,31 @@ def max_altitude(act):
     return highest_pt
 
 def peak_detector(df, gain_threshold):
-    df.rename(columns={'alt': 'altitude', 'lat': 'position_lat', 'lon': 'position_long'}, inplace=True) 
+    """[Filters activities df retaining activities where the gain 
+        in altitude is more than the specified threshold]
+    Args:
+        df ([pandas dataframe]): [activities df]
+        gain_threshold ([float]): [filter df keeping activities 
+        where altitude gain more than this threshold]
+    Returns:
+        [pandas dataframe]: [df containing activities where the 
+        altitude gain is great than the specified gain threshold]
+    """
+    #df.rename(columns={'alt': 'altitude', 'lat': 'position_lat', 'lon': 'position_long'}, inplace=True) 
     with_alt = df.dropna(subset=['altitude'])
     peaks = with_alt.groupby('activity_id').apply(max_altitude)
     peaks = peaks[peaks['gain'] > gain_threshold]
     return peaks
 
 def plot_one_cluster(m, df, activities_ids, steps, color):
+    """[Adds plots lat/long position for each activity 
+        in the list of activities_ids to a folium map]
+    Args:
+        m ([folium map object]): [a folium map object where the plot]
+        df ([pandas dataframe]): [df with colums position_lat, position_long, and activity_id]
+        activities_ids ([list]): [list of activity_id for which you wish to plot the path taken]
+        steps ([int]): [plots every steps-th lat/long]
+    """
     for activity_id in activities_ids:
         locations = df.query(f'activity_id=={activity_id}')[['position_lat', 'position_long']].iloc[::steps]
         locations.dropna(inplace=True)
@@ -26,6 +53,16 @@ def plot_one_cluster(m, df, activities_ids, steps, color):
         folium.PolyLine(points, color=color, weight=2.5, opacity=0.5).add_to(m)
 
 def plot_multiple_clusters(df, peaks, colormap, cluster_series, steps):   
+    """[Plots multiple clusters of peaks on a folium map]
+    Args:
+        df ([pandas dataframe]): [df with colums position_lat, position_long, and activity_id]
+        peaks ([pandas dataframe]): [df with peaks lat/long]
+        colormap ([array]): [array of colors to use]
+        cluster_series ([type]): [description]
+        steps ([int]): [plots every steps-th lat/long]
+    Returns:
+        [folium map object]: [folium map object]
+    """
     central_lat, central_long = peaks.query(f'cluster=={cluster_series.idxmax()}')[['position_lat', 'position_long']].mean()
     n = len(list(cluster_series.index))
     m = folium.Map(location=[central_lat, central_long],tiles='Stamen Terrain', zoom_start=12)
@@ -39,6 +76,14 @@ def plot_multiple_clusters(df, peaks, colormap, cluster_series, steps):
     return m
 
 def peak_clustering(peaks, epsilon, min_samples=2):
+    """[Clusters activities]
+    Args:
+        peaks ([pandas dataframe]): [df with peaks lat/long]
+        epsilon ([float]): [parameter for DBSCAN local radius for expanding clusters]
+        min_samples (int, optional): [minimun number of samples for cluster]. Defaults to 2.
+    Returns:
+        [pandas series]: [index is cluster number, values are number of activities belonging to that cluster]
+    """
     clustering = DBSCAN(eps=epsilon, metric='haversine', min_samples=min_samples).fit(peaks[['position_lat', 'position_long']])
     peaks['cluster'] = clustering.labels_ 
     frequency = peaks['cluster'].value_counts().sort_values(ascending=False)
@@ -46,6 +91,19 @@ def peak_clustering(peaks, epsilon, min_samples=2):
     return cluster_series
 
 def save_cluster_plot(person, epsilon, colormap, filename, gain_threshold=400, steps=10):
+    """[Saves the clusterplot]
+    Args:
+        person ([string]): [The name of the folder in ../data 
+        where activity files are.]
+        epsilon ([float]): [parameter for DBSCAN local radius for expanding clusters]
+        colormap ([array]): [array of colors to use]
+        filename ([string]): [name of the output file]
+        gain_threshold (float, optional): [filter df keeping activities 
+        where altitude gain more than this threshold]. Defaults to 400.
+        steps (int, optional): [plots every steps-th lat/long]. Defaults to 10.
+    Returns:
+        [pandas series]: [index is cluster number, values are number of activities belonging to that cluster]
+    """
     person_df = pd.read_parquet(f'../data/{person}/df.parquet')
     peaks = peak_detector(person_df, gain_threshold)
     peaks = peaks.dropna(subset=['position_lat', 'position_long'])
@@ -60,11 +118,8 @@ if __name__ == '__main__':
             '#cab2d6','#6a3d9a','#ffff99','#b15928']
     colormap = itertools.cycle(colors)
     
-    person = 'KM_Strava'
-    epsilon = 0.0009 #0.0005
-    s = save_cluster_plot(person, epsilon, colormap, 'Summit')
-    print(s)
+    epsilon = 0.001 
 
-    person = 'BL_Strava'
-    # b = save_cluster_plot(person, epsilon, colormap, 'Boulder')
-    # print(b)
+    person = 'Example_Strava'
+    b = save_cluster_plot(person, epsilon, colormap, 'Example')
+    print(b)
--- src/pipeline_to_parquet.py
+++ src/pipeline_to_parquet.py
@@ -5,9 +5,22 @@ import os
 import pandas as pd
 
 def semicir_to_degs(semicirc):
+    """[Converts from semicircle to degrees]
+    Args:
+        semicirc ([float]): [lat or long in semicircular units]
+    Returns:
+        [float]: [lat or long in degree units]
+    """
     return semicirc * (180 / 2**31)
 
 def parse_fit(handle):
+    """[Parses fit files]
+    Args:
+        handle: [the handle of the file to be parsed]
+    Returns:
+        [pandas dataframe]: [dataframe with columns from recorded data
+                              converts semicircle lat/long to degrees lat/long]
+    """
     try:
         fitfile = FitFile(handle)
         df = pd.DataFrame([{d['name']: d['value'] for d in r.as_dict()['fields']} 
@@ -19,6 +32,13 @@ def parse_fit(handle):
         print(f'Issue reading fit file {handle}')
 
 def parse_gpx(handle):
+    """[Parses gpx files]
+    Args:
+        handle: [the handle of the file to be parsed]
+    Returns:
+        [pandas dataframe]: [dataframe with columns of timestamp, position_lat
+                            position_long(in degrees), 'altitude']
+    """
     gpx = gpxpy.parse(handle)
     track_coords = []
     for track in gpx.tracks:
@@ -29,6 +49,12 @@ def parse_gpx(handle):
     return df
 
 def parse_file(filename):
+    """[Parses file method depends on file type]
+    Args:
+        filename: [the name of the file you want to parse]
+    Returns:
+        [dataframe]: [see output of corresponding parser above]
+    """
     if filename.endswith('.fit.gz'):
         handle = gzip.open(filename)
         return parse_fit(handle)
@@ -42,18 +68,40 @@ def parse_file(filename):
         print(f'Add parser for {filename} to parse_file function.')
 
 def full_path(directory, filename):
+    """[Joins directory and filename to give full path to the file]
+    Args:
+        directory: [directory name]
+        filename: [filename]
+    Returns:
+        [string]: [full path to the file]
+    """
     try:
         return os.path.join(f'../data/{directory}/', filename)
     except Exception as e:
         print(f'Failed to join {filename} in {directory}')
     
 def check_file_exists(file):
+    """[Checks to see if file exists]
+    Args:
+        file: [filepath]
+    Returns:
+        [boolean]: [returns true if filepath exists]
+    """
     try:
         return os.path.exists(file)
     except Exception as e:
         print(f'File {file} does not exists')
         
 def get_activities(directory):
+    """[Gets overview of activity data from the csv file
+       creates a pandas dataframe, processes columns, checks
+       to see if filename corresponding to activity exists
+       returns df sorted by activity data]
+    Args:
+        directory: [directory]
+    Returns:
+        [pandas dataframe]: [df of activity overview]
+    """
     df = pd.read_csv(f'../data/{directory}/activities.csv')
     df = df[['Activity ID', 'Activity Date', 'Activity Name', 'Activity Type', 
                              'Elapsed Time', 'Distance', 'Filename', 'Moving Time',
@@ -65,27 +113,31 @@ def get_activities(directory):
     return df.sort_values('activity_date')
 
 def parquet_activities(person):
-        act_df = get_activities(person)
-        act_df['person'] = person
-        print(f'Reading {len(act_df)} activities for {person}. Please be patient as this might take a while!')
-        dfs = []
-        for i, t in enumerate(act_df.to_dict(orient='records')):
-            if i % 40 == 0:
-                print(f'{round(100*i/len(act_df),0)} % complete')
-            try:
-                tmp = parse_file(t['filename'])
-                tmp['activity_id'] = t['activity_id']
-                tmp['person'] = t['person']
-                dfs.append(tmp)
-            except Exception as e:
-                pass
-        print('Creating parquet file')
-        df = pd.concat(dfs)
-        df['timestamp'] = pd.to_datetime(df['timestamp'].map(lambda x: str(x)[:19]))
-        df.to_parquet(f'../data/{person}/df.parquet')
+    """[Creates a parquet file of the dataframe]
+    Args:
+        person ([string]): [The name of the folder in ../data 
+        where the activity overview file is.]
+    """
+    act_df = get_activities(person)
+    act_df['person'] = person
+    print(f'Reading {len(act_df)} activities for {person}. Please be patient as this might take a while!')
+    dfs = []
+    for i, t in enumerate(act_df.to_dict(orient='records')):
+        if i % 40 == 0:
+            print(f'{round(100*i/len(act_df),0)} % complete')
+        try:
+            tmp = parse_file(t['filename'])
+            tmp['activity_id'] = t['activity_id']
+            tmp['person'] = t['person']
+            dfs.append(tmp)
+        except Exception as e:
+            pass
+    print('Creating parquet file')
+    df = pd.concat(dfs)
+    df['timestamp'] = pd.to_datetime(df['timestamp'].map(lambda x: str(x)[:19]))
+    df.to_parquet(f'../data/{person}/df.parquet')
 
 if __name__ == "__main__":
 
-    people =  ['MB_Strava', 'BL_Strava', 'KM_Strava', 'LB_Strava', 'BK_Strava']
-    parquet_activities(people[3])
+    parquet_activities('Example_Strava')
 
