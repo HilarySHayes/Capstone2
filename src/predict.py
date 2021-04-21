9874758d0ca8c2fb1c358b3a7a665b00e877c8dd
Hilary Hayes <sarneski@gmail.com>
2021-04-20 20:01:24 -0600

    Added docustrings

--- src/predict.py
+++ src/predict.py
@@ -1,15 +1,32 @@
+from branca.element import Template, MacroElement
 import folium
+from functools import partial
 from geopy import distance
 import numpy as np
 import pandas as pd
 import pickle
+import legend_helper
 
-def resample_activity(df):
-    return df.resample("30s").mean().drop('activity_id', axis='columns')
+def resample_activity(df, interval="30s"):
+    """[resamples from the dataframe]
+    Args:
+        df ([pandas dataframe]): [df you will sample from]
+        interval ([string]): [interval at which you want to resample]
+    Returns:
+        [pandas dataframe]: [dataframe resampled at the specified 
+        interval averaged over the interval]
+    """
+    return df.resample(interval).mean().drop('activity_id', axis='columns')
 
-def compute_speed_distance(tmp):
-    df = tmp.copy()
-    df.reset_index(inplace=True)
+def compute_speed_distance(df):
+    """[Takes df with position_lat, position_long, altitude, and time and 
+        calculates change in altitude, time, distance, speed]
+    Args:
+        df ([pandas dataframe]): [df with position_lat, position_long, altitude, and time]
+    Returns:
+        [pandas dataframe]: [df with distance, change in time, change in altitude, speed]
+    """
+    df = df.reset_index()
     df['lon_previous'] = df['position_long'].shift(1)
     df['lat_previous'] = df['position_lat'].shift(1)
     df['alt_previous'] = df['altitude'].shift(1)
@@ -23,41 +40,64 @@ def compute_speed_distance(tmp):
     return df
 
 def window_features(df, speed_thres=0, window=2*5):
+    """[From dataframe with speed, distance, time, and change in altitude 
+        calculates mean, max, and std of these over a rolling window]
+    Args:
+        df ([pandas dataframe]): [dataframe with speed, distance, time, and change in altitude]
+        speed_thres (int, optional): [Threshold for speed. Only keeps rows when
+                                       speed is >= this threshold]. Defaults to 0.
+        window (int, optional): [size of the moving window over which you
+                                 are calculating. Length of time is affected by
+                                 resampling]. Defaults to 2*5, when resampling is 30s
+                                 this is a window of 5 min.
+    Returns:
+        [pandas df]: [a windowed dataframe]
+    """
     df = df.query(f"speed >= {speed_thres}")
     month = df['time'].dt.month
     dist = df['distance'].rolling(window).mean()
     dist_max = df['distance'].rolling(window).max()
     dist_std = df['distance'].rolling(window).std()
-    
     speed = df['speed'].rolling(window).mean()
     speed_max = df['speed'].rolling(window).max()
     speed_std = df['speed'].rolling(window).std()
-    
     delta_alt = df['delta_alt'].rolling(window).mean()
     delta_alt_max = df['delta_alt'].rolling(window).max()
     delta_alt_std = df['delta_alt'].rolling(window).std()
-  
     final_df = pd.concat([dist, dist_max, dist_std, 
                     speed, speed_max, speed_std,
                     delta_alt, delta_alt_max, delta_alt_std, month], axis=1)
-    
     final_df.columns = ['dist', 'dist_max', 'dist_std', 
                   'speed', 'speed_max', 'speed_std',
                   'delta_alt', 'delta_alt_max', 'delta_alt_std', 'month']
     return final_df
 
-def evaluate_mode(filename, clf, speed_thres=0, window=2*5):
+def evaluate_mode(filename, clf, speed_thres=0, window=2*5, interval="30s"):
+    """[Imports activity, converts to dataframe, predicts activity type
+        for each segment of activity]
+    Args:
+        filename ([string]): [filepath]
+        clf ([model]): [fit classification model]
+        speed_thres (int, optional): [Threshold for speed. Only keeps rows when
+                                       speed is >= this threshold]. Defaults to 0.
+        window (int, optional): [size of the moving window over which you
+                                 are calculating. Length of time is affected by
+                                 resampling]. Defaults to 2*5, when resampling is 30s
+                                 this is a window of 5 min.
+        interval ([string]): [interval at which you want to resample] Defaults to 30s
+    Returns:
+        [pandas df]: [dataframe for activity including predictions of activity
+                      type for each segment]
+    """
     sample_df = pd.read_parquet(filename)
-
     sample_df['time'] = pd.to_datetime(sample_df['time'], utc=True)
     sample_df['time'] = sample_df['time'].dt.tz_localize(tz=None)
     sample_df.set_index('time', inplace=True)
-
-    sample_df = sample_df.groupby(['activity_id']).apply(resample_activity).reset_index().set_index('time')
+    resample_activity_instance = partial(resample_activity, interval=interval)
+    sample_df = sample_df.groupby(['activity_id']).apply(resample_activity_instance).reset_index().set_index('time')
     sample_df = sample_df.fillna(method='ffill')
     sample_df = sample_df.fillna(method='bfill')
     sample_df = compute_speed_distance(sample_df)
-
     sample_data = window_features(sample_df, speed_thres, window)
     sample_data = sample_data.dropna()
     X = sample_data.values
@@ -65,9 +105,17 @@ def evaluate_mode(filename, clf, speed_thres=0, window=2*5):
     return sample_df[['time', 'position_lat', 'position_long', 'predicted_mode']].dropna(axis=0)
 
 def visualize_prediction(df, color_dict, save_to):
+    """[Creates folium map of activity path colored by predicted activity type]
+    Args:
+        df ([pandas df]): [dataframe of activity data including position_lat, position_long
+                            and predicted_mode, the predicted activity type]
+        color_dict ([dictionary]): [keys are activity types, values are colors]
+        save_to ([string]): [filepath where folium map is saved]
+    Returns:
+        [folium.Map object]: [folium map of activity path colored by predicted activity type]
+    """
     df['break'] = df['predicted_mode']==df['predicted_mode'].shift(1)
     df['segment'] = None
-
     segment = 0
     rows = []
     for row in df.to_dict(orient='records'):
@@ -76,30 +124,30 @@ def visualize_prediction(df, color_dict, save_to):
             segment += 1
         rows.append(row)            
     df = pd.DataFrame(rows).fillna(method='ffill')
-    
+
     central_lat = df.position_lat.mean()
     central_long = df.position_long.mean()
-
     m = folium.Map(location=[central_lat,central_long],tiles="Stamen Terrain",zoom_start=14.5)
-
     for segment in df.segment.unique():
         locations = df.query(f"segment=={segment}")[['position_lat', 'position_long']]
         act_type = df.query(f"segment=={segment}")[['predicted_mode']].values[0][0]
         folium.PolyLine(locations.values.tolist(), color=color_dict[act_type], weight=2.5, opacity=0.9).add_to(m)
+    macro = MacroElement()
+    macro._template = Template(legend_helper.legend_string)
+    m.add_child(macro)
     m.save(save_to)
     return m
 
 if __name__ == '__main__':
     with open('../data/model.pkl', 'rb') as f:
         clf = pickle.load(f)
-    activity_id = 76248711 #561475718
+    activity_id = 4408957556
     filename = f'../data/samples/sample{activity_id}.parquet'
-    # filename= '../data/R_data/act5.parquet'
     final_df = evaluate_mode(filename, clf)
     activity_types = final_df.predicted_mode.unique()
     print(activity_types)
-    color_dict = {'fly_down':'#e41a1c', 'run_down':'#377eb8', 
-                    'run_up':'#4daf4a', 'ski_up': '#984ea3',
+    color_dict = {'fly_down':'#e41a1c', 'run_down':'#ffff33', 
+                    'run_up':'#377eb8', 'ski_up': '#984ea3', 
                     'ski_down': '#ff7f00'}
     save_to = f'../images/predictions_for_{activity_id}.html'
     visualize_prediction(final_df, color_dict, save_to)
